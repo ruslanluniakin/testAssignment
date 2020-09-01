eb171f043beae5a8ea509ec20fff2216f7d85134
Лунякин Руслан Александрович <lun1998lun@gmail.com>
2020-09-01 11:29:22 +0300

    added tests

--- testAssignment.Tests/TestTestController.cs
+++ testAssignment.Tests/TestTestController.cs
@@ -16,16 +16,31 @@ namespace testAssignment.Tests
         {
             // Arrange
             var mock = new Mock<IRepository>();
-            mock.Setup(repo => repo.GetCars(It.IsAny<Owner>())).Returns(GetTestCars());
+            mock.Setup(repo => repo.GetCars(It.IsAny<Owner>())).Returns(GetTestCars(new Owner{ FirstName = "Говард", LastName = "Зимин" }));
             var controller = new TestController(mock.Object);
 
             // Act
-            var result = controller.Post(new Owner { FirstName = "Говард", LastName = "Зимин" });
+            var result = controller.Post(new Owner ());
 
             // Assert
             var okResult = result as OkObjectResult;
             var cars = okResult.Value as List<Car>;
-            Assert.Equal(GetTestCars().Count, cars.Count);
+            Assert.Equal(GetTestCars(new Owner { FirstName = "Говард", LastName = "Зимин" }).Count, cars.Count);
+        }
+
+        [Fact]
+        public void GetCarsReturnsNotFoundResult()
+        {
+            // Arrange
+            var mock = new Mock<IRepository>();
+            mock.Setup(repo => repo.GetCars(It.IsAny<Owner>())).Returns(GetTestCars(new Owner { FirstName = "Василида", LastName = "Симонова" }));
+            var controller = new TestController(mock.Object);
+
+            // Act
+            var result = controller.Post(new Owner());
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
         }
 
         [Fact]
@@ -33,7 +48,7 @@ namespace testAssignment.Tests
         {
             // Arrange
             var mock = new Mock<IRepository>();
-            mock.Setup(repo => repo.GetOwners(It.IsAny<int>())).Returns(GetTestOwners());
+            mock.Setup(repo => repo.GetOwners(It.IsAny<int>())).Returns(GetTestOwners(2));
             var controller = new TestController(mock.Object);
 
             // Act
@@ -42,23 +57,48 @@ namespace testAssignment.Tests
             // Assert
             var okResult = result as OkObjectResult;
             var owners = okResult.Value as List<Owner>;
-            Assert.Equal(GetTestCars().Count, owners.Count);
+            Assert.Equal(GetTestOwners(2).Count, owners.Count);
         }
 
-        private List<Car> GetTestCars()
+        [Fact]
+        public void GetOwnersReturnsNotFoundResult()
         {
-            var cars = new List<Car>
+            // Arrange
+            var mock = new Mock<IRepository>();
+            mock.Setup(repo => repo.GetOwners(It.IsAny<int>())).Returns(GetTestOwners(100));
+            var controller = new TestController(mock.Object);
+
+            // Act
+            var result = controller.Get(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        private List<Car> GetTestCars(Owner owner)
+        {
+            if (owner.LastName == "Зимин" && owner.FirstName == "Говард")
             {
-                new Car() { CarBrand = "Toyota", Model = "RAV4", Color = "Красный", YearOfProduction = new DateTime(2012, 1, 1) },
-                new Car() { CarBrand = "Honda", Model = "Accord", Color = "Белый", YearOfProduction = new DateTime(2011, 1, 1) },
-                new Car() { CarBrand = "Volkswagen", Model = "Tiguan", Color = "Желтый", YearOfProduction = new DateTime(2017, 1, 1) },
-                new Car() { CarBrand = "Ford", Model = "Focus", Color = "Черный", YearOfProduction = new DateTime(2010, 1, 1) },
-            };
-            return cars;
+                var cars = new List<Car>
+                { 
+                    new Car() { CarBrand = "Toyota", Model = "RAV4", Color = "Красный", YearOfProduction = new DateTime(2012, 1, 1) },
+                    new Car() { CarBrand = "Honda", Model = "Accord", Color = "Белый", YearOfProduction = new DateTime(2011, 1, 1) },
+                    new Car() { CarBrand = "Volkswagen", Model = "Tiguan", Color = "Желтый", YearOfProduction = new DateTime(2017, 1, 1) },
+                    new Car() { CarBrand = "Ford", Model = "Focus", Color = "Черный", YearOfProduction = new DateTime(2010, 1, 1) },
+                };
+                return cars;
+            }
+
+            return null;
         }
 
-        private List<Owner> GetTestOwners()
+        private List<Owner> GetTestOwners(int? id)
         {
+            if (id < 1 || id > 10)
+            {
+                return null;
+            }
+
             var owners = new List<Owner>
             {
                 new Owner() { LastName = "Лучная", FirstName = "Анора", MiddleName = "Владиславовна", BirthYear = new DateTime(1996, 5, 18) },
